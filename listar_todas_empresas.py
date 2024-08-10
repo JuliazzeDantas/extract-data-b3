9c42130c7e8674257e9fbfddf1bd4c8a4bdb7d75
root <root@N-5BHB424.cosan.rede>
2024-08-10 14:13:50 -0300

    Usar o bs4 para extrair os dados do dadosdemercado

--- listar_todas_empresas.py
+++ listar_todas_empresas.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from pydantic import BaseModel
+
 from ativo_modelo import Acoes
 
 acoes=''
